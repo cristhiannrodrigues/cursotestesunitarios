941b0ff30eae04b908090e3995c4f0047aa49eb6
Cristhiann Rodrigues <cristhiannrodrigues@gmaiil.com>
2022-07-03 17:06:44 -0300

    Asserts

---
+++ src/test/java/AssertTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssertTest {
+
+    @Test
+    public void test() {
+
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+        Assert.assertEquals(1, 1);
+
+    }
+
+}
--- src/test/java/br/ce/cristhiann/servicos/LocacaoServiceTtest.java
+++ src/test/java/br/ce/cristhiann/servicos/LocacaoServiceTtest.java
@@ -4,10 +4,14 @@ import br.ce.cristhiann.entidades.Filme;
 import br.ce.cristhiann.entidades.Locacao;
 import br.ce.cristhiann.entidades.Usuario;
 import br.ce.cristhiann.utils.DataUtils;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class LocacaoServiceTtest {
 
@@ -23,9 +27,9 @@ public class LocacaoServiceTtest {
         Locacao locacao = service.alugarFilme(usuario, filme);
 
         //verificacao
-        Assert.assertTrue(locacao.getValor() == 5.0);
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+        assertThat(locacao.getValor(), CoreMatchers.is(5.0));
+        assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 
     }
 
