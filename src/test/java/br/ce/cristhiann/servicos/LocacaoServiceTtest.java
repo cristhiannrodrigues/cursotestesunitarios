bb5026b43df1e8d382fb5a83a02b44a3167c7d92
Cristhiann Rodrigues <cristhiannrodrigues@gmaiil.com>
2022-07-03 17:12:58 -0300

    ErrorCollector

--- src/test/java/br/ce/cristhiann/servicos/LocacaoServiceTtest.java
+++ src/test/java/br/ce/cristhiann/servicos/LocacaoServiceTtest.java
@@ -6,14 +6,20 @@ import br.ce.cristhiann.entidades.Usuario;
 import br.ce.cristhiann.utils.DataUtils;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class LocacaoServiceTtest {
+
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
 
     @Test
     public void teste() {
@@ -27,9 +33,9 @@ public class LocacaoServiceTtest {
         Locacao locacao = service.alugarFilme(usuario, filme);
 
         //verificacao
-        assertThat(locacao.getValor(), CoreMatchers.is(5.0));
-        assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-        assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+        error.checkThat(locacao.getValor(), is(4.0));
+        error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(false));
 
     }
 
